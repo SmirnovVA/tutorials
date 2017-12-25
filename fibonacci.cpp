12a2d95070b45b2887e2107ccae13ae90fbbfa88
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2017-12-25 13:12:04 +0300

    second task

---
+++ fibonacci.cpp
@@ -0,0 +1,16 @@
+int fibonacciRecursion(int n){
+	if(n == 1) return 1;
+	else if (n <= 0) return 0;	
+	return (fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2));
+}
+
+int fibonacciNoRecursion(int n){
+	if(n == 1 || n == 2) return 1;
+	int prevFib = 1, resFib = 1, temp =0;
+	for(int i = 2; i < n ; i++){
+		temp = resFib + prevFib;
+		prevFib = resFib;
+		resFib = temp;
+	}
+	return resFib;
+}
