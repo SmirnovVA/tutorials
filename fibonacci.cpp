e84881ad0b1e7cf371f3070a8ad32a554aec3a63
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2017-12-26 10:20:39 +0300

    error correction

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,6 +1,6 @@
 int fibonacciRecursion(int n){
 	if(n == 1) return 1;
-	else if (n <= 0) return 0;	
+	else if (n == 0) return 0;	
 	return (fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2));
 }
 
