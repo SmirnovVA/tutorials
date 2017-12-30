6fa5185c594817c514230357126ada8a89f74c5a
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2017-12-30 18:38:06 +0300

    second correction(third task)

--- Simmetry.cpp
+++ Simmetry.cpp
@@ -14,13 +14,10 @@ bool axisOfSymmetry(int x[n], int y[n]){
 	if (y[minPoint] != y[maxPoint])	return false; // axis of symmetry doesn't exist	because 2 extreme points doesn't reflects to each other
 	else aos = (x[maxPoint] + x[minPoint])/2; //  x-value of axis
 	for(i = 0; i < n; i++){ //check if aos is suitable for other points
-		xMin = x[i];
-		if(xMin == aos) continue;// if point is on axis -> it reflects to itself -> go to next iteration
+		if(x[i] == aos) continue;// if point is on axis -> it reflects to itself -> go to next iteration
 		else{
-			if(xMin < aos) xMax = xMin + 2 * (aos - xMin);// if point is left from aos -> pair is right from aos
-			else if(xMin > aos) xMax = xMin - 2 * (xMin - aos); // if point is right from aos -> pair is left from aos
-			for(j = 0; j < n; j++){
-				if(x[j] == xMax){
+			for(j = 0; j < n; j++){ //search for pair to point with equal y-value		
+				if(x[j] == (2 * aos - x[i])){
 					if(y[i] == y[j]) continue;// if pair was found -> if y-value equal go to next iteration
 				}
 				if(j == n - 1) return false;// if pair with equal y-value wasn't found -> axis of symmetry doesn't exist
