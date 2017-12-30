f11c135207334733e78bfdae98bd1e3e37aaa302
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2017-12-30 19:08:42 +0300

    add O(n) solution

--- Simmetry.cpp
+++ Simmetry.cpp
@@ -1,6 +1,6 @@
 double aos = 0.0;
 
-bool axisOfSymmetry(int x[n], int y[n]){
+bool axisOfSymmetry(int x[n], int y[n]){ //O(n^2)
 	int maxPoint, minPoint, xMax, xMin;
 	xMin = xMax = 0;
 	for(int i = 0; i < n; i++){	// search for min and max x in massive
@@ -23,6 +23,30 @@ bool axisOfSymmetry(int x[n], int y[n]){
 				if(j == n - 1) return false;// if pair with equal y-value wasn't found -> axis of symmetry doesn't exist
 			}
 		}
+	}
+	return true;
+}
+
+
+std::set<std::pair<int, int>> setOfPoints;
+
+bool axisOfSymmetry2(int x[n], int y[n]){// O(n)
+	int maxPoint, minPoint, xMax, xMin;
+	xMin = xMax = 0;
+	for(int i = 0; i < n; i++){	// search for min and max x in massive
+		if(x[i] > xMax) {
+			maxPoint = i
+		}
+		else if(x[i] < xMin){ 
+			minPoint = i
+		}
+		setOfPoints.insert({x[i], y[i]});
+	}
+	if (y[minPoint] != y[maxPoint])	return false; // axis of symmetry doesn't exist	because 2 extreme points doesn't reflects to each other
+	else aos = (x[maxPoint] + x[minPoint])/2; //  x-value of axis
+	for(i = 0; i < n; i++){ //check if aos is suitable for other points		
+		if(setOfPoints.exist({2 * aos - x[i],y[i]})) continue;
+		else return false;
 	}
 	return true;
 }
