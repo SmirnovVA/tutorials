352f116e3d0a3a1f3826bdccb00a6154efa64c64
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-09 10:46:09 +0300

    first correction (fifth task)

--- maxsum.cpp
+++ maxsum.cpp
@@ -1,18 +1,20 @@
 int resultSum = 0;
+int segmentBegin, segmentEnd;
 
 void maxSum(int x[], int size){
 	resultSum = x[0];
 	int currentSum = 0;
-	int segmentBegin = 0, segmentEnd;
+	int newSegmentBegin = 0;
 	for(int i = 0; i < size; i++){
 		currentSum += x[i];
 		if(currentSum >= resultSum){
 			resultSum = currentSum;
+			segmentBegin = newSegmentBegin;
 			segmentEnd = i;
 		}
 		if(currentSum < 0){
 			currentSum = 0;
-			segmentBegin = i + 1;
+			newSegmentBegin = i + 1;
 		}
 	}
 }
