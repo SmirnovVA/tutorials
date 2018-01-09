2a9e492ff71eb8c0a1502fb70a3daf53ced5c821
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-09 14:02:36 +0300

    error correction

--- brackets.cpp
+++ brackets.cpp
@@ -1,11 +1,11 @@
-bool isBracketsCorrect(inString inString){
+bool isBracketsCorrect(srd::string inString){
 	std::stack<char> brackets;
 	for(int i = 0; i < inString.length(); i ++){
-		if(inString.at(i) == '{' || inString.at(i) == '[' || inString.at(i) == '(') brackets.push(inString.at(i));
 		if((inString.at(i) == '}' && brackets.top() != '{') ||
 		   (inString.at(i) == ']' && brackets.top() != '[') ||
-		   (inString.at(i) == '(' && brackets.top() != ')')) return false
+		   (inString.at(i) == ')' && brackets.top() != '(')) return false
 		else brackets.pop();
+		brackets.push(inString.at(i))
 	}
 	return brackets.empty();
 }
