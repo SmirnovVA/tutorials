444221390e59528dacf916a1dd9ce49c1997c64a
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-10 10:50:49 +0300

    error correction

--- brackets.cpp
+++ brackets.cpp
@@ -8,7 +8,7 @@ bool isBracketsCorrect(std::string inString){
 	for(int i = 0; i < inString.length(); i ++){
 		it = bracketTypes.begin();
 		while(it->second != inString.at(i)) it++;//search for value inString.at(i) in map 'bracketTypes'
-		if(it != bracketTypes.end()) brackets.push();//if value was found push it to stack 'brackets'
+		if(it != bracketTypes.end()) brackets.push(inString.at(i));//if value was found push inString.at(i) it to stack 'brackets'
 		if(bracketTypes.count(inString.at(i)) && 
 		   brackets.top() != bracketTypes.find(inString.at(i))->second) return false;//if map 'bracketTypes' contains element with key = inString.at(i) 
 											     //&& stack 'brackets' top element != value of map element with key = inString.at(i) )
