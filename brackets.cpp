254542a6912c2d6f06e52b752d6cc966d5325804
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-10 10:43:33 +0300

    code review

--- brackets.cpp
+++ brackets.cpp
@@ -1,14 +1,19 @@
-bool isBracketsCorrect(srd::string inString){
+bool isBracketsCorrect(std::string inString){
 	std::stack<char> brackets;
+	std::map<char,char> bracketTypes;
+	std::map<char,char>::iterator it;
+	bracketTypes['}']='{';
+	bracketTypes[']']='[';
+	bracketTypes[')']='(';
 	for(int i = 0; i < inString.length(); i ++){
-		if((inString.at(i) == '}' && brackets.top() != '{') ||
-		   (inString.at(i) == ']' && brackets.top() != '[') ||
-		   (inString.at(i) == ')' && brackets.top() != '(')) return false
-		else{
-			brackets.pop();
-			continue;
-		}
-		brackets.push(inString.at(i))
+		it = bracketTypes.begin();
+		while(it->second != inString.at(i)) it++;//search for value inString.at(i) in map 'bracketTypes'
+		if(it != bracketTypes.end()) brackets.push();//if value was found push it to stack 'brackets'
+		if(bracketTypes.count(inString.at(i)) && 
+		   brackets.top() != bracketTypes.find(inString.at(i))->second) return false;//if map 'bracketTypes' contains element with key = inString.at(i) 
+											     //&& stack 'brackets' top element != value of map element with key = inString.at(i) )
+											     //-> sequence is incorrect
+		else brackets.pop();							     // else pop top element from stack 'brackets'	
 	}
 	return brackets.empty();
 }
