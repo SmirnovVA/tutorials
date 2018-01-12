c993b6a4d5ed10c2de13acf3e0e55a81ebaee3d9
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-12 14:27:22 +0300

    error correction(seven task)

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,10 +1,11 @@
 bool isPalindrom(std::string inString){
 	std::string str1;
 	std::string str2;
+	std::set<char> str;
 	int len = inString.length();
 	for(int i = 0; i < len; i++){
-		if(inString[i] != ' ') str1.push_back(inString[i]);
-		if(inString[len - i - 1] != ' ') str2.push_back(inString[len - i - 1]);
+		if(inString[i] != ' ') str1.push_back(std::toupper(inString[i]));
+		if(inString[len - i - 1] != ' ') str2.push_back(std::toupper(inString[len - i - 1]));
 	}
 	return (str1.compare(str2) == 0);
 }
