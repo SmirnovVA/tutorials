4cc1b6904401ca13e69e39bd9bcf29851864ed57
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-12 13:38:09 +0300

    code review(seven task)

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,9 +1,10 @@
 bool isPalindrom(std::string inString){
 	std::string str1;
 	std::string str2;
-	for(int i = 0; i < inString.length(); i++){
+	int len = inString.length();
+	for(int i = 0; i < len; i++){
 		if(inString[i] != ' ') str1.push_back(inString[i]);
-		if(inString[inString.length() - i - 1] != ' ') str2.push_back(inString[inString.length() - i - 1]);
+		if(inString[len - i - 1] != ' ') str2.push_back(inString[len - i - 1]);
 	}
 	return (str1.compare(str2) == 0);
 }
