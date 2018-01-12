dd02c682a9fc0dbb2b85be2f3ce178a6c9235a4d
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-12 13:25:31 +0300

    error correction

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,7 +1,7 @@
 bool isPalindrom(std::string inString){
 	std::string str1;
 	std::string str2;
-	for(int i = 0; i < inString.length()/2; i++){
+	for(int i = 0; i < inString.length(); i++){
 		if(inString[i] != ' ') str1[i] = inString[i];
 		if(inString[inString.length() - i - 1] != ' ') str2[i] = inString[inString.length() - i - 1];
 	}
