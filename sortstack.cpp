9dbc620e48739ce8b83a7db1582daa3f6bcc75cf
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-01-20 02:12:59 +0300

    error correction(eight task)

--- sortstack.cpp
+++ sortstack.cpp
@@ -1,23 +1,22 @@
-std::stack<int> sortedStack;
-
-void sortStackToMax(std::stack<int> stack){
+std::stack<int> sortStackToMax(std::stack<int> stack){
 	std::stack<int> tempStack;
+	std::stack<int> sortedStack;
 	int currentMin;
-	while(!stack.empty()){//do main cycle while all elements from first stack will not go to result stack
+	while(!stack.empty()){//do main cycle while all elements from  stack will not go to result stack
 		currentMin = stack.top();
-		while(!stack.empty()){//find min in first stack
+		while(!stack.empty()){//find min in  stack
+			if(stack.top() < currentMin) currentMin = stack.top();// set new min in stack		
+			tempStack.push(stack.top());//push stack top to temp
 			stack.pop();
-			if(stack.top() < currentMin) 
-			{
-				currentMin = stack.top();
-				continue;				
-			}
-			tempStack.push(stack.top());//push first stack to temp
 		}
-		sortedStack.push(currentMin);//push min element from stack to result stack
-		while(!tempStack.empty()){//push elements from temp stack to first stack
-			stack.push(tempStack.top());		//push elements to first stack	
+		while(!tempStack.empty()){//push elements from temp stack to stack
+			if(tempStack.top() == currentMin){//if temp stack equal to currentMin -> push it to sorted stack
+				sortedStack.push(tempStack.top());
+				tempStack.pop();
+			}
+			stack.push(tempStack.top());//push other elements to stack	
 			tempStack.pop();
 		}
 	}
+	return sortedStack;
 }
