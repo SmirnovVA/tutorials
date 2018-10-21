1b37667e8c6d41f22d69bab17bb5b0bb2f0ab177
SmirnovVA <34808280+SmirnovVA@users.noreply.github.com>
2018-10-21 12:27:25 +0300

    some task
    something i dont know

--- sortstack.cpp
+++ sortstack.cpp
@@ -2,20 +2,22 @@ std::stack<int> sortStackToMax(std::stack<int> stack){
 	std::stack<int> tempStack;
 	std::stack<int> sortedStack;
 	int currentMin;
-	while(!stack.empty()){//do main cycle while all elements from  stack will not go to result stack
+	while (!stack.empty()) {//do main cycle while all elements from  stack will not go to result stack
 		currentMin = stack.top();
-		while(!stack.empty()){//find min in  stack
-			if(stack.top() < currentMin) currentMin = stack.top();// set new min in stack		
+		while (!stack.empty()) {//find min in  stack
+			if (stack.top() < currentMin) currentMin = stack.top();// set new min in stack		
 			tempStack.push(stack.top());//push stack top to temp
 			stack.pop();
 		}
-		while(!tempStack.empty()){//push elements from temp stack to stack
-			if(tempStack.top() == currentMin){//if temp stack equal to currentMin -> push it to sorted stack
+		while (!tempStack.empty()) {//push elements from temp stack to stack
+			if (tempStack.top() == currentMin) {//if temp stack equal to currentMin -> push it to sorted stack
 				sortedStack.push(tempStack.top());
 				tempStack.pop();
 			}
-			stack.push(tempStack.top());//push other elements to stack	
-			tempStack.pop();
+			else {
+				stack.push(tempStack.top());//push other elements to stack	
+				tempStack.pop();
+			}
 		}
 	}
 	return sortedStack;
